fac6aed15be1c71f7867f244c0de6193ee40c214
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-10-22 20:56:36 +0200

    First implementation of tree insert / remove for OTS

--- src/main/java/org/opentrafficsim/spatialtree/test/ShortMergePerformance.java
+++ src/main/java/org/opentrafficsim/spatialtree/test/ShortMergePerformance.java
@@ -1,10 +1,24 @@
 package org.opentrafficsim.spatialtree.test;
 
+import java.rmi.RemoteException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.naming.NamingException;
 
 import org.djunits.value.vdouble.scalar.Duration;
 import org.djunits.value.vdouble.scalar.Time;
+import org.djutils.event.EventInterface;
+import org.djutils.event.EventListenerInterface;
 import org.opentrafficsim.core.dsol.OtsSimulator;
+import org.opentrafficsim.core.geometry.OtsShape;
+import org.opentrafficsim.core.gtu.Gtu;
+import org.opentrafficsim.core.network.Link;
+import org.opentrafficsim.road.network.OtsRoadNetwork;
+import org.opentrafficsim.road.network.lane.CrossSectionLink;
+import org.opentrafficsim.road.network.lane.Lane;
+import org.opentrafficsim.spatialtree.SpatialTree;
+import org.opentrafficsim.spatialtree.h2.SpatialTreeH2;
 import org.opentrafficsim.spatialtree.test.ShortMerge.ShortMergeModel;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
@@ -19,18 +33,31 @@ import nl.tudelft.simulation.dsol.SimRuntimeException;
  * @author <a href="https://tudelft.nl/staff/p.knoppers-1">Peter Knoppers</a>
  * @author <a href="https://dittlab.tudelft.nl">Wouter Schakel</a>
  */
-public class ShortMergePerformance
+public class ShortMergePerformance implements EventListenerInterface
 {
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /** the network. */
+    private OtsRoadNetwork network;
+    
+    /** the tree to use. */
+    private SpatialTree tree;
+    
     /**
      * Create a class to compare the performance of different spatial tree implementations for a model.
      */
     public ShortMergePerformance()
     {
+        this.tree = new SpatialTreeH2(); 
         try
         {
             OtsSimulator simulator = new OtsSimulator("ShortMerge");
             final ShortMergeModel otsModel = new ShortMergeModel(simulator);
+            this.network = otsModel.getNetwork();
             simulator.initialize(Time.ZERO, Duration.ZERO, Duration.instantiateSI(3600.0), otsModel);
+            addInfra();
+            subscribeGtus();
             simulator.start();
             while (simulator.getSimulatorTime().si < 3600.0)
             {
@@ -42,7 +69,51 @@ public class ShortMergePerformance
         {
             exception.printStackTrace();
         }
+    }
 
+    private void addInfra()
+    {
+        for (Link link : this.network.getLinkMap().values())
+        {
+            CrossSectionLink csl = (CrossSectionLink) link;
+            for (Lane lane : csl.getLanes())
+            {
+                this.tree.add(lane);
+            }
+        }
+    }
+
+    private void subscribeGtus()
+    {
+        this.network.addListener(this, OtsRoadNetwork.GTU_ADD_EVENT);
+        this.network.addListener(this, OtsRoadNetwork.GTU_REMOVE_EVENT);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void notify(final EventInterface event) throws RemoteException
+    {
+        if (event.getType().equals(OtsRoadNetwork.GTU_ADD_EVENT))
+        {
+            String gtuId = event.getContent().toString();
+            Gtu gtu = this.network.getGTU(gtuId);
+            gtu.addListener(this, Gtu.MOVE_EVENT);
+        }
+        else if (event.getType().equals(OtsRoadNetwork.GTU_REMOVE_EVENT))
+        {
+            String gtuId = event.getContent().toString();
+            Gtu gtu = this.network.getGTU(gtuId);
+            gtu.removeListener(this, Gtu.MOVE_EVENT);
+            this.tree.remove(gtu);
+        }
+        else if (event.getType().equals(Gtu.MOVE_EVENT))
+        {
+            Object[] contentArray = (Object[]) event.getContent();
+            String gtuId = contentArray[0].toString();
+            Gtu gtu = this.network.getGTU(gtuId);
+            this.tree.remove(gtu);
+            this.tree.add(gtu);
+        }
     }
 
     /**
